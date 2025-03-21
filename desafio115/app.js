2356f35d66b9bdcc2176e5c53ce933b10264270b
AngeloSouza1 <angeloafdesouza@gmail.com>
2025-03-21 08:24:49 -0300

    create project

---
+++ desafio115/app.js
@@ -0,0 +1,9 @@
+function reverterOrdemPalavras(frase) {
+    return frase.split(' ').reverse().join(' ');
+}
+
+// Exemplo de uso
+const frase = "viva no mundo da codificação";
+const resultado = reverterOrdemPalavras(frase);
+
+console.log(`🧙‍♂️ Frase encantada: "${resultado}" ✨`);
