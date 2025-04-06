6be1bdb870efff9bc4d9e7ae4f44b76f4d4fe95c
AngeloSouza1 <angeloafdesouza@gmail.com>
2025-04-06 13:00:00 -0300

    update project

--- testes/desafio135.js
+++ testes/desafio135.js
@@ -1,12 +1,11 @@
 function detectorDeSabores(salgadinhos) {
-    // Usamos Set para remover duplicatas e depois ordenamos com sort
-    const saboresUnicos = [...new Set(salgadinhos)].sort((a, b) => a - b);
-    return saboresUnicos;
-  }
-  
-  // Exemplo de uso:
-  const coleta = [8, 3, 4, 8, 2, 3, 4, 10, 10];
-  const resultado = detectorDeSabores(coleta);
-  
-  console.log("🛸 Tipos de salgadinhos coletados (sem repetições):", resultado);
-  
+  // Usamos Set para remover duplicatas e depois ordenamos com sort
+  const saboresUnicos = [...new Set(salgadinhos)].sort((a, b) => a - b);
+  return saboresUnicos;
+}
+
+// Exemplo de uso:
+const coleta = [8, 3, 4, 8, 2, 3, 4, 10, 10];
+const resultado = detectorDeSabores(coleta);
+
+console.log("🛸 Tipos de salgadinhos coletados (sem repetições):", resultado);
