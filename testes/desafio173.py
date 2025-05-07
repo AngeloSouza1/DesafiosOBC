3e95e4b7c400163de1e65dd8dc59a67cfc39f4d2
AngeloSouza1 <angeloafdesouza@gmail.com>
2025-05-07 15:00:00 -0300

    feat: inclusao projeto

---
+++ testes/desafio173.py
@@ -0,0 +1,7 @@
+def ordenar_feiticos(feiticos):
+    feiticos_ordenados = [''.join(sorted(f)) for f in feiticos]
+    return sorted(feiticos_ordenados)
+
+entrada = ['wxus', 'soacb']
+saida = ordenar_feiticos(entrada)
+print(saida)  # ['abcos', 'suwx']
