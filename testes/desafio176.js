5e626122031c06d153476b26d0ab27652c10bb1a
AngeloSouza1 <angeloafdesouza@gmail.com>
2025-05-11 15:00:00 -0300

    feat: inclusao projeto

---
+++ testes/desafio176.js
@@ -0,0 +1,11 @@
+function ordenarPalavrasMagicas(palavras) {
+    return palavras.sort();
+  }
+  
+
+  const palavrasMagicas = ['abracadabra', 'zippity', 'foo', 'magic', 'spell'];
+  const resultado = ordenarPalavrasMagicas(palavrasMagicas);
+  
+  console.log("🔮 Ordem das Palavras Mágicas:");
+  console.log(resultado);
+  
